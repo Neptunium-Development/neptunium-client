e3cf9769d60e50cd0f46be929f2b36e35e2120fb
heedii <127703762+heedii@users.noreply.github.com>
2023-04-15 13:07:57 -0700

    trying to fix

--- src/main/java/me/nep/cl/client/NepClient.java
+++ src/main/java/me/nep/cl/client/NepClient.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-public class VapeMenuClient implements ClientModInitializer {
+public class NepClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {}
