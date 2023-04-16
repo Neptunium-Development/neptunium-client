344051a836450d6504afde74da64634f224d7fad
heedii <127703762+heedii@users.noreply.github.com>
2023-04-15 17:59:21 -0700

    Created Sprint Module

---
+++ src/main/java/me/corruptionhades/vapemenu/module/impl/movement/Sprint.java
@@ -0,0 +1,20 @@
+package me.corruptionhades.vapemenu.module.impl.movement;
+
+import me.corruptionhades.vapemenu.event.EventTarget;
+import me.corruptionhades.vapemenu.event.impl.EventUpdate;
+import me.corruptionhades.vapemenu.module.Category;
+import me.corruptionhades.vapemenu.module.Module;
+
+import static me.corruptionhades.vapemenu.utils.PacketHelper.mc;
+
+public class Sprint extends Module {
+
+    public Sprint() {
+        super("Sprint", "Keeps Sprint", Category.MOVEMENT);
+    }
+
+    @EventTarget
+    public void onUpdate(EventUpdate e) {
+        mc.player.setSprinting(true);
+    }
+}
